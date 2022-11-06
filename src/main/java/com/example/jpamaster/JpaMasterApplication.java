4bc53f0f0b4952abff3cf6408c5bc7851759da22
eunjis <ejejc@naver.com>
2022-11-06 15:19:42 +0900

    initial commit

---
+++ src/main/java/com/example/jpamaster/JpaMasterApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpamaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaMasterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jpamaster/JpaMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpamaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaMasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
