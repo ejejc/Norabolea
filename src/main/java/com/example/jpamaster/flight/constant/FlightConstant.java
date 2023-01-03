ec023835003a4c31a60104bd4d1ebaf8326c2129
ansehyeong <dkstpgud@gmail.com>
2023-01-03 21:24:38 +0900

    fix : airplane 등록 로직 변경 - airschedule 에 연결 되어야 할 엔티티 제거 및 등록 로직 수정

---
+++ src/main/java/com/example/jpamaster/flight/constant/FlightConstant.java
@@ -0,0 +1,6 @@
+package com.example.jpamaster.flight.constant;
+
+public class FlightConstant {
+    public static final int AVERAGE_AIRPLANE_SPEED = 900;
+    public static final int EARTH_RADIUS = 6371;
+}
