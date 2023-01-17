b60cc3f46aadb2e6dcb6450d8ea7c9fb54bce77f
ansehyeong <dkstpgud@gmail.com>
2023-01-17 09:15:17 +0900

    feat : 구 둘레 측정을 위한 하버사인 알고리즘 추가 및 테스트 -> 기존 알고리즘과 오차율이 무의미한 수치이기 때문에 기존 알고리즘 사용

--- src/test/java/com/example/jpamaster/flight/util/FlightUtilsTest.java
+++ src/test/java/com/example/jpamaster/flight/util/FlightUtilsTest.java
@@ -4,9 +4,21 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class FlightUtilsTest {
+
+    private double fromLat = 37.4692;
+    private double fromLon = 126.451;
+
+    private double toLat = 34.4272222;
+    private double toLon = 135.244167;
+
+    private double toLat2 = 49.1966913;
+    private double toLon2 = -123.1815123;
 
     @Test
     void dateTimeTest() {
@@ -16,4 +28,14 @@ class FlightUtilsTest {
         Assertions.assertThat(expectedTime).isAfter(now);
     }
 
+    @Test
+    void haversiveFomulaDistanceAsKm() {
+        int distanceAsKmToJapan = FlightUtils.getDistanceAsKm(fromLat, fromLon, toLat, toLon);
+        int distanceAsKmToCanada = FlightUtils.getDistanceAsKm(fromLat, fromLon, toLat2, toLon2);
+        double haversiveFomulaDistanceAsKmToJapan = FlightUtils.haversiveFomulaDistanceAsKm(fromLat, fromLon, toLat, toLon);
+        double haversiveFomulaDistanceAsKmToCanada = FlightUtils.haversiveFomulaDistanceAsKm(fromLat, fromLon, toLat2, toLon2);
+
+        Assertions.assertThat(distanceAsKmToJapan).isEqualTo((int) Math.ceil(haversiveFomulaDistanceAsKmToJapan));
+        Assertions.assertThat(distanceAsKmToCanada).isEqualTo((int) Math.ceil(haversiveFomulaDistanceAsKmToCanada));
+    }
 }
