30cd65635870d3ac87c484c09a73b717e74fd8c7
ansehyeong <dkstpgud@gmail.com>
2023-02-26 23:02:28 +0900

    fix : initializing 변경 및 spring security 의존성 추가

---
+++ src/main/java/com/example/jpamaster/common/config/SecurityConfig.java
@@ -0,0 +1,8 @@
+package com.example.jpamaster.common.config;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+}
