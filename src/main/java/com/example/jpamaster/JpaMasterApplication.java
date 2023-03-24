168370613571ff371aebab52864c0d8eae3b7e41
ansehyeong <dkstpgud@gmail.com>
2023-03-25 01:00:57 +0900

    fix: spring auto configuration annotation 패키지 변경 및 각 configuration 클래스로 변경

--- src/main/java/com/example/jpamaster/JpaMasterApplication.java
+++ src/main/java/com/example/jpamaster/JpaMasterApplication.java
@@ -8,9 +8,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "com.example.jpamaster")
-@EntityScan(basePackages = "com.example.jpamaster")
-@EnableJpaRepositories(basePackages = "com.example.jpamaster")
-@EnableFeignClients
 public class JpaMasterApplication {
 
     public static void main(String[] args) {
