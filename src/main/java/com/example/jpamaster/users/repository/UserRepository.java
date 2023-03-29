8b442887102ea602d4d447934ac32259c4f3a651
최은지 <>
2023-03-29 20:58:35 +0900

    회원 아이디 조회 추가.

--- src/main/java/com/example/jpamaster/users/repository/UserRepository.java
+++ src/main/java/com/example/jpamaster/users/repository/UserRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
+    Optional<User> findById(String id);
 }
